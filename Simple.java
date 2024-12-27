b39049de25eced4ef01ff7ae1d75de521e9b87e5
Shifa Nadaf <125539242+Shifanadaf@users.noreply.github.com>
2024-12-27 22:59:10 +0530

    Update Simple.java

--- Simple.java
+++ Simple.java
@@ -1,5 +1,7 @@
-class Simple{  
-    public static void main(String args[]){  
-     System.out.println("Hello Java");  
-    }  
-}  
+public class Simple 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
